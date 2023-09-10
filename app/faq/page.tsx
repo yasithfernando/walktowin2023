336c03e8621c6782e179c86c81d710abd962c06c
yasithfernando <iyfernando42@gmail.com>
2023-09-11 01:47:48 +0530

    production release

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,8 +1,70 @@
+import { soloMarks, teamMarks } from "@/constants";
 
 async function Page(){
-   return(
-     <div className="text-light-2">FAQ</div>
-   )
+   return (
+    <div className="flex justify-center items-center p-4">
+      <div className="max-w-2xl mx-auto p-6 rounded-lg shadow text-light-2">
+        <h1 className="text-2xl font-semibold mb-4">Walk To Win 2023 - Rules & Regulations</h1>
+
+        <h2 className="text-xl font-semibold mb-2">Objectives</h2>
+        <ul className="list-disc pl-6 mb-4">
+          <li>Motivate employees to maintain their health prior to the Annual Medical checkup</li>
+          <li>Motivate employees to focus more on their health</li>
+          <li>Motivate employees who are finding it difficult to maintain a healthy lifestyle</li>
+        </ul>
+
+        <h2 className="text-xl font-semibold mb-2">Rules & Regulations</h2>
+        <ul className="list-disc pl-6 mb-4">
+          <li>The competition will be held for two weeks from the announced date (11 September)</li>
+          <li>The competition will be held in two categories "Team" & "Individual"</li>
+          <li>Each team category requires four (4) Members</li>
+          <li>It is mandatory to have one(1) female member in each team</li>
+          {/* Add more rules here */}
+        </ul>
+
+        <h2 className="text-xl font-semibold mb-2">Point System</h2>
+        <div className="mb-4">
+          <h3 className="text-lg font-semibold mb-2">Team Marks</h3>
+          <table className="table-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">Steps Count</th>
+                <th className="px-4 py-2">Points</th>
+              </tr>
+            </thead>
+            <tbody>
+              {teamMarks.map(([stepsCount, points], index) => (
+                <tr key={index}>
+                  <td className="px-4 py-2">{stepsCount}</td>
+                  <td className="px-4 py-2">{points}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div className="mb-4">
+          <h3 className="text-lg font-semibold mb-2">Solo Marks</h3>
+          <table className="table-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">Steps Count</th>
+                <th className="px-4 py-2">Points</th>
+              </tr>
+            </thead>
+            <tbody>
+              {soloMarks.map(([stepsCount, points], index) => (
+                <tr key={index}>
+                  <td className="px-4 py-2">{stepsCount}</td>
+                  <td className="px-4 py-2">{points}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 
--- components/shared/LeaderboardItem.tsx
+++ components/shared/LeaderboardItem.tsx
@@ -1,5 +1,5 @@
 import Player from '@/constants/player';
-import React from 'react';
+import React, { use, useEffect, useState } from 'react';
 
 import {
   Popover,
@@ -7,48 +7,136 @@ import {
   PopoverTrigger,
 } from "@/components/radix-ui/Popover"
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/radix-ui/Accordion"
+import { apiURLs } from '@/constants';
+
+
 
 
 interface Props {
-  player: any;
-  index: number
+  player: any,
+  index: number,
+  isTeam: boolean
 }
 
-const LeaderboardItem = ({player, index}:Props) => {
-  const randomAvatar =`https://source.boringavatars.com/beam/50/${player.name.split(" ")[0]}%20${player.name.split(" ")[1]}?colors=582770,773D94,943D8A,C22760,E81764`
+const LeaderboardItem = ({player, index, isTeam}:Props) => {
+
+  const [totalSteps, setTotalSteps] = useState([{}]);
+  const [teamMembers, setTeamMembers] = useState([{}]);
+
+  async function getPlayerHistory(){
+    const response = await fetch(`${apiURLs.getAnyPlayer}${player.id}`);
+    const data = await response.json();
+    const totalSteps = data.total_steps;
+    return totalSteps;
+  }
+
+  async function getTeamHistory(){
+    const response = await fetch(`${apiURLs.getTeam}${player._id}`);
+    const data = await response.json();
+    const teamData = data;
+    return teamData;
+  }
+
+  useEffect(() => {
+    if(isTeam){
+      getTeamHistory().then((teamData)=>{
+        setTeamMembers(teamData);
+      }
+      )
+    }
+    else{
+      getPlayerHistory().then((totalSteps)=>{
+        setTotalSteps(totalSteps);
+      }
+      )
+    }
+  }
+  , [])
+
+
+  const randomAvatar =`https://source.boringavatars.com/beam/50/${player.name.split(" ")[0]}%20${player.name.split(" ")[1]}?colors=582770,773D94,943D8A,C22760,E81764`;
   return (
-      <Popover>
-        <div className="flex items-center p-4 text-light-2 cursor-pointer mt-1 pl-0">
-          <div className='grid content-center bg-gradient-to-tl from-violet-500 to-fuchsia-500 w-10 lg:w-14 h-8 rounded-l-full pl-0 lg:pl-2 -mr-3.5 lg:-mr-4 '>
-            <div className='w-8 flex justify-center text-center'>
-              <p className='text-light-2 text-subtle-semibold lg:text-base-semibold'>{index+1}</p>
-            </div>
-          </div>
-        <div className="w-10 h-10 lg:w-12 lg:h-12 mr-4">
-            <img src={player.profileImage ? player.profileImage : randomAvatar} alt={`Avatar of ${player.name}`} className="w-full h-full rounded-full border-2 border-fuchsia-400" />
-        </div>
-        <div className="flex-grow gap-1">
+      <Accordion type="single" collapsible>
         
-            <PopoverTrigger>
-                <p className="text-lg font-semibold">{player.name}</p>
-                <p className='text-subtle-semibold text-left'>{player.steps}</p>
-            </PopoverTrigger>
-            <PopoverContent>
-                <div className='bg-white w-full h-40'>
+        <AccordionItem value={`item-1`}>
+          
 
+          
+            <div className=" w-full flex items-center text-light-2 cursor-pointer mt-1 pl-0 justify-between">
+              <div className=' flex flex-row '>
+                <div className='flex flex-row items-center justify-center'>
+                  <div className='grid content-center bg-gradient-to-tl from-violet-500 to-fuchsia-500 w-10 lg:w-14 h-8 rounded-l-full pl-0 lg:pl-2 -mr-3.5 lg:-mr-4 '>
+                    <div className='w-8 flex justify-center text-center'>
+                      <p className='text-light-2 text-subtle-semibold lg:text-base-semibold'>{index+1}</p>
+                    </div>
+                  </div>
+                  <div className="w-10 h-10 lg:w-12 lg:h-12 mr-4">
+                    <img src={player.profileImage ? player.profileImage : randomAvatar} alt={`Avatar of ${player.name}`} className="w-full h-full rounded-full border-2 border-fuchsia-400" />
+                  </div>
                 </div>
-
-            </PopoverContent>
+                <div className="">
+                    <p className="text-lg font-semibold">{player.name}</p>
+                    <p className='text-subtle-semibold text-left'>{player.steps}</p>
+                </div>
+              </div>
+              <AccordionTrigger>
+                <div className="">
+                  <p className='text- text-body-semibold'>{player.points}</p>
+                </div>
+              </AccordionTrigger>
+            </div>
+          <AccordionContent>
+            <div className='w-full'>
+                <ul>
+                  {
+                    totalSteps.map((item:any,index)=>{
+                      if(!item.date){
+                        return null;
+                      }
+                      return(
+                        <li key={index} className='flex flex-row justify-between pr-8 pl-8'>
+                          <span className='text-light-2 text-subtle-medium'>{item.date.split("T")[0]}</span>
+                          <span className='text-light-2 text-subtle-medium'>{item.steps}</span>
+                        </li>
+                      )
+                    }
+                    )
+                  }
+                </ul>
+                <ul>
+                  {
+                    teamMembers.map((item:any, index)=>{
+                      return(
+                        <li key={index} className='flex flex-row justify-between pr-8 pl-8'>
+                          <span className='text-light-2 text-subtle-medium'>{item.name}</span>
+                          <span className='text-light-2 text-subtle-medium'>{item.steps}</span>
+                        </li>
+                      )
+                    }
+                    )
+                  }
+                </ul>
+              
+              
+            </div>
+            
+          </AccordionContent>
+          
         
-        </div>
-        <div className="flex flex-row gap-2">
-            
-            <p className='text-base-semibold'>{player.points}</p>
-        </div>
+        
+        
+        </AccordionItem>
+        
+        
             
 
-        </div>
-      </Popover>
+      </Accordion>
 
   );
 };
