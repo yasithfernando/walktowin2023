273f2c84e3c43cc37d95b08efb432b0dcabfcae8
yasithfernando <iyfernando42@gmail.com>
2023-09-10 17:30:17 +0530

    updated build

--- lib/gapi.ts
+++ lib/gapi.ts
@@ -38,39 +38,6 @@ export async function getAccessToken(gmail: string, code: string) {
     const oAuth2Client = await getOAuthClient(gmail);
 
     const { tokens } = await oAuth2Client.getToken(code);
-    oAuth2Client.setCredentials(tokens);
     return tokens;
-}
-
-
-export async function refreshAccessToken(refreshToken: string, gmail: string) {
-  const oAuth2Client = await getOAuthClient(gmail);
-
-  // Set the refresh token on the client
-  oAuth2Client.setCredentials({ refresh_token: refreshToken });
-
-  try {
-    // Request a new access token using the refresh token
-    const response = await oAuth2Client.refreshAccessToken().then((res) => {
-      console.log("res", res);
-      return res;
-    });
-
-    // The new access token is in tokens.access_token
-    //const newAccessToken = tokens.access_token;
-
-    // Optionally, you can also receive a new refresh token, but it might remain the same
-    //const newRefreshToken = tokens.refresh_token;
-
-    // Store the new access token (and possibly the new refresh token) securely
-    // for future use, replacing the old ones
-    // ...
-
-    //return newAccessToken;
-    return response;
-  } catch (error) {
-    console.error("Error refreshing access token:", error);
-    throw error;
-  }
 }
 
